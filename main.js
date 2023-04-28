cf3c4c7eb3db6a8078835f1d86a49b0d9c58d3ad
Tayan21 <bostybaev@gmail.com>
2023-04-28 17:04:21 +0600

    finish keyboard

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let spaceKey = document.querySelector('.space_key');
 let shift_left = document.querySelector('.shift_left');
 let shift_right = document.querySelector('.shift_right');
 let caps_lock_key = document.querySelector('.caps_lock_key');
+let textarea = document.querySelector('.text')
 
 for (let i = 0; i < keys.length; i++) {
   keys[i].setAttribute('keyname', keys[i].innerText);
@@ -15,6 +16,24 @@ window.addEventListener('keydown', function(e) {
       console.log(keys[i])
       keys[i].classList.add('active')
     }
+    console.log(e.code)
+    if (!(e.code == 'Tab')) {
+
+      textarea.value += e.key 
+    }
+   
+    if (e.code == 'Space') {
+      spaceKey.classList.add('active')
+    }
+    if (e.code == 'ShiftLeft') {
+      shift_right.classList.remove('active')
+    }
+    if (e.code == 'ShiftRight') {
+      shift_left.classList.remove('active')
+    }     
+    if (e.code == "CapsLock") {
+      caps_lock_key.classList.add('active')
+    }
   }
 })
 
@@ -25,8 +44,23 @@ window.addEventListener('keyup', function(e) {
       keys[i].classList.remove('active')
       keys[i].classList.add('remove')
     }
+    if (e.code == 'Space') {
+      spaceKey.classList.remove('active')
+      spaceKey.classList.add('remove')
+    }
+    if (e.code == 'ShiftLeft') {
+      shift_right.classList.remove('active')
+      shift_right.classList.remove('remove')
+    }
+    if (e.code == 'ShiftRight') {
+      shift_left.classList.remove('active')
+      shift_left.classList.remove('remove')
+    }
+    if (e.code == 'CapsLock') {
+      caps_lock_key.classList.remove('active')
+    }  
     setTimeout(() => {
-      keys[i].remove(remove)
-    })
+      keys[i].classList.remove('remove')
+    }, 200)
   }
 })
